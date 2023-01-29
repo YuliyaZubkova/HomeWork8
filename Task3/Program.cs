d07fdf33c5d0c58e4f3303eaa8276b93960c85bf
Yuliya <i@yzubkova.ru>
2023-01-30 00:54:48 +0300

    Частотный словарь - как часто встречается число в одномерном массиве

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿/* Частотный словарь - проверяет как часто встречается элемент в массиве данных*/
+
+int [] array = new int [] {1, 9, 2, 3, 4, 5, 2, 4, 3, 1, 4, 6, 4, 3, 2};
+
+Dictionary<int, int> dic = new Dictionary<int, int>();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(dic.ContainsKey(array[i]))
+        dic[array[i]] = dic[array[i]] + 1;
+    else 
+        dic.Add(array[i], 1);
+}
+foreach (var item in dic.OrderBy(x=>x.Key))
+{
+    Console.WriteLine($"{item.Key} встречается {item.Value} раз.");
+}
