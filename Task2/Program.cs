bd8611a210e666051786b90883c390942d5f975f
Yuliya <i@yzubkova.ru>
2023-01-29 22:54:22 +0300

    Меняем строки на столбцы в массиве или выдаем что замена невозможна

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу которая заменяет строки на столбцы
+Если это невозможно программа должна вывести сообщение для пользователя
+*/
+
+// Получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено не число.");
+
+        }
+    }
+
+    return result;
+}
+
+//Задать матрицу и заполнить ее числами с консоли
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+//Распечатать массив
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            if (matrix.GetLength(0) == matrix.GetLength(1))
+            {
+                Console.Write($"{matrix[i, j]} ");
+            }
+        Console.WriteLine();
+    }
+}
+
+//Замена строк на столбцы
+void PrintNewMatrix(int[,] matrix)
+{
+    if (matrix.GetLength(0) != matrix.GetLength(1))
+    {
+        Console.WriteLine($"Замена невозможна, так как введено разное количество строк и столбцов.");
+    }
+    else
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[j, i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+int m = GetNumber("Введите количество строк: ");
+int n = GetNumber("Введите количество столбцов: ");
+
+int[,] matrix = GetMatrix(m, n);
+
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintNewMatrix(matrix);
+
+
+
+
