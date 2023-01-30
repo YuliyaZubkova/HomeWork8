2fe1b723006e3fdaeef935a6c59be0e44c167de5
Yuliya <i@yzubkova.ru>
2023-01-31 00:59:58 +0300

    Спиральный массив 4х4

--- HomeTask5/Program.cs
+++ HomeTask5/Program.cs
@@ -5,3 +5,44 @@
 11 16 15 06
 10 09 08 07
 */
+
+Console.Clear();
+int n = 4;
+int[,] Matrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= Matrix.GetLength(0) * Matrix.GetLength(1))
+{
+    Matrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < 3)
+        j++;
+    else
+    if (i < j && i + j >= 3)
+        i++;
+    else
+    if (i >= j && i + j > 3)
+        j--;
+    else
+        i--;
+}
+
+WriteMatrix(Matrix);
+
+void WriteMatrix(int[,] matrix)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (matrix[i, j] / 10 <= 0)
+                Console.Write($" {matrix[i, j]} ");
+
+            else Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
