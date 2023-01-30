9746c0dd9096e39695edad23f9d2fa2a42f9d65c
Yuliya <i@yzubkova.ru>
2023-01-30 11:38:08 +0300

    добавлена задача 5 со второго семинара

---
+++ TASK_05/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа 
+и проверяет является ли одно число квадратом другого
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+
+int number1 = GetNumber("Введите первое число: ");
+int number2 = GetNumber("Введите второе число: ");
+
+if (number1*number1 == number2 || number2*number2 == number1)
+Console.WriteLine("Да");
+else Console.WriteLine($"Нет");
