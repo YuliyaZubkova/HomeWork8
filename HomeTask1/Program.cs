21352fcd1abd784a12c49c4e374581f903523ca3
Yuliya <i@yzubkova.ru>
2023-01-30 15:07:33 +0300

    Строки матрицы отсортированы по убыванию

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -10,3 +10,93 @@
 8 4 4 2
 */
 
+// Получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено не число.");
+
+        }
+    }
+
+    return result;
+}
+
+//Задать матрицу и заполнить ее числами с консоли
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+//Распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// отсортировать числа в строках по убыванию и вывести на экран отсортированную матрицу
+void NewMatrix(int[,] matrix)
+{
+    int accum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int sort = 0; sort < matrix.GetLength(1) - 1; sort++)
+            {
+                if (matrix[i, sort] < matrix[i, sort + 1])
+                {
+                    accum = matrix[i, sort + 1];
+                    matrix[i, sort + 1] = matrix[i, sort];
+                    matrix[i, sort] = accum;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите количество строк: ");
+int n = GetNumber("Введите количество столбцов: ");
+
+int[,] matrix = GetMatrix(m, n);
+
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+NewMatrix(matrix);
+Console.WriteLine();
+
