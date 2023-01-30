bfd9eb5f0d3c0d19a4362171269cc25085ba3c00
Yuliya <i@yzubkova.ru>
2023-01-30 10:50:35 +0300

    добавлена задача 2 со второго семинара с функцией

--- TASK_02/Program.cs
+++ TASK_02/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int GetNumber()
+{
+    Random rnd = new Random();
+    int result = rnd.Next(100, 1000);
+    return result;
+}
+
+int DeleteSecond(int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 10;
+    int res = firstDigit * 10 + secondDigit;
+    return res;
+}
+
+
+
+int number = GetNumber();
+Console.Write($"Заданное число: {number}");
+int res = DeleteSecond(number);
+Console.Write($" -> {res}");
